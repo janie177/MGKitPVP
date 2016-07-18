008d6902e523d643ae1874f4dca5885c41a62cfd
Jan <janie177@hotmail.com>
2016-07-18 21:07:53 +0200

    Added boost framework completely. Next is adding the actual boosts.

--- src/main/java/net/minegusta/heropvp/boosts/Boost.java
+++ src/main/java/net/minegusta/heropvp/boosts/Boost.java
@@ -1,22 +1,46 @@
 package net.minegusta.heropvp.boosts;
 
+import com.google.common.collect.Maps;
+import org.bukkit.Material;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentMap;
+
 public enum Boost {
-	TICKETS30(0, "+30% Tickets", "Get 30% extra tickets.", 15, 250),
-	HEALTHPOT(6, "Healthy", "Get a health splash potion on spawn.", 15, 100);
+	TICKETS30(0, "+30% Tickets", "Get 30% extra tickets.", 15, 250, Material.PAPER, 1),
+	HEALTHPOT(6, "Healthy", "Get a health splash potion on spawn.", 15, 100, Material.INK_SACK, 1);
 
 	private int slot;
 	private String name;
 	private String description;
+	private byte data;
 	private int duration;
 	private int cost;
+	private Material material;
 
-	Boost(int slot, String name, String description, int duration, int cost)
+	Boost(int slot, String name, String description, int duration, int cost, Material material, int data)
 	{
 		this.slot = slot;
 		this.name = name;
+		this.material = material;
+		this.data = (byte) data;
 		this.description = description;
 		this.duration = duration;
 		this.cost = cost;
+	}
+
+	private static ConcurrentMap<Integer, Boost> indexes = Maps.newConcurrentMap();
+
+	static {
+		for (Boost boost : Boost.values()) {
+			indexes.put(boost.slot, boost);
+		}
+	}
+
+	public static Optional<Boost> getForSlot(int slot)
+	{
+		if(indexes.containsKey(slot)) return Optional.of(indexes.get(slot));
+		return Optional.empty();
 	}
 
 	public int getSlot() {
@@ -37,5 +61,13 @@ public enum Boost {
 
 	public int getCost() {
 		return cost;
+	}
+
+	public Material getMaterial() {
+		return material;
+	}
+
+	public byte getData() {
+		return data;
 	}
 }
--- src/main/java/net/minegusta/heropvp/gui/BoostShop.java
+++ src/main/java/net/minegusta/heropvp/gui/BoostShop.java
@@ -1,5 +1,7 @@
 package net.minegusta.heropvp.gui;
 
+import com.google.common.collect.Lists;
+import net.minegusta.heropvp.boosts.Boost;
 import net.minegusta.heropvp.main.Main;
 import net.minegusta.heropvp.saving.MGPlayer;
 import net.minegusta.mglib.gui.InventoryGUI;
@@ -9,6 +11,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+import java.util.Optional;
+
 public class BoostShop extends InventoryGUI {
 
 	public BoostShop(String name, int rows, String key) {
@@ -20,19 +28,56 @@ public class BoostShop extends InventoryGUI {
 		Inventory inv = Bukkit.createInventory(inventoryHolder, i, ChatColor.DARK_PURPLE + "Boost!");
 		MGPlayer mgp = Main.getSaveManager().getMGPlayer(player);
 
-		//TODO build the inventory, fill will all items in the Boost enum.
-		//TODO Then check if the player has the boost active or not. Display all info on the item.
-		//TODO Add item material in the boost enum.
 
-		//Maybe lock the boost when already active?
-
+		for(Boost boost : Boost.values())
+		{
+			if(mgp.hasBoost(boost))
+			{
+				long left = mgp.getMinutesLeft(boost);
+				inv.setItem(boost.getSlot(), new ItemStack(boost.getMaterial(), (int) (left > 64 ? 64 : left), boost.getData())
+				{
+					{
+						ItemMeta meta = getItemMeta();
+						meta.setDisplayName(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + boost.getName());
+						List<String> lore = Lists.newArrayList(ChatColor.YELLOW + "" + ChatColor.BOLD + "ACTIVE", ChatColor.GREEN + boost.getDescription(), ChatColor.GRAY + "Time Left: " + ChatColor.RED + boost.getDuration() + ChatColor.GRAY + " Minutes");
+						meta.setLore(lore);
+						setItemMeta(meta);
+					}
+				});
+			}
+			else
+			{
+				inv.setItem(boost.getSlot(), new ItemStack(boost.getMaterial(), 1, boost.getData())
+				{
+					{
+						ItemMeta meta = getItemMeta();
+						meta.setDisplayName(ChatColor.YELLOW + "" + ChatColor.BOLD + boost.getName());
+						List<String> lore = Lists.newArrayList(ChatColor.LIGHT_PURPLE + boost.getDescription(), ChatColor.GRAY + "Duration: " + ChatColor.RED + boost.getDuration() + ChatColor.GRAY + " Minutes", ChatColor.YELLOW + "" + ChatColor.BOLD + "Cost: " + ChatColor.GREEN + boost.getCost() + ChatColor.YELLOW + " Tickets.");
+						meta.setLore(lore);
+						setItemMeta(meta);
+					}
+				});
+			}
+		}
 		return inv;
 	}
 
 	@Override
 	public void processClick(Player player, int i, InventoryClickEvent inventoryClickEvent) {
+		Optional<Boost> oBoost = Boost.getForSlot(i);
+		if (oBoost.isPresent()) {
+			Boost boost = oBoost.get();
+			MGPlayer mgp = Main.getSaveManager().getMGPlayer(player);
 
-		//TODO Get boost for slot. Then apply boost with cost if the player has tickets.
+			if(mgp.hasBoost(boost))
+			{
+				return;
+			}
+			else
+			{
+				mgp.addboostSeconds(boost, boost.getDuration() * 60);
+			}
+		}
 
 
 	}
